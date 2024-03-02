732927e071c09bf7c60ee5fac4e4840e899386a4
rdbhand <rdbhand308@gmail.com>
2024-03-02 21:07:41 +0530

    First Commit

---
+++ CPP/ADS/bt.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+class Family
+{
+    public:
+    string name;
+    Family *left,*right;
+    void create();
+    void insert(Family *root,Family *next);
+    void inorder()
+}*root=NULL;
+
+
+int main()
+{
+
+    return 0;
+}
